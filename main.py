cf93f382142cc81df3a4dd4589f4ff3d2acb1240
mathryciuk <mat.hryciuk@gmail.com>
2023-03-28 19:02:13 +0200

    zadania

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #...
+
+liczby = [-1,34,-45,2,45,-79989,12,345,12,-46,23,-456,99]
+slow = {1:"100",2:"200",3:"300"}
+
+#  odzielnie plik z funkcjami , rozwiązania i dane w pliku main.py
+# funkcja która z podanej listy drukuje tylko liczby dodatnie 
+# funkcja która drukuje liczby z listy podzielne przez 5
+# fukcję która zwraca nową listę z liczbami mnniejszymi od zera 
+# funkcję która wydrukuje element z podanego klucza 
