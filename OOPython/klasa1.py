aeaf8977f0842a5f5768e2df6905d5bcec0845d7
mathryciuk <mat.hryciuk@gmail.com>
2023-04-12 18:50:01 +0200

    klasa1

---
+++ OOPython/klasa1.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+class Zakupy:
+
+    
+    koszyk = []
+    
+    def __init__(self,nazwa,rabat):
+        self.nazwa = nazwa
+        self.data = str(datetime.now().date())
+        self.godzina = str(datetime.now().strftime("%H:%M:%S"))
+        self.rabat = rabat
+        
+    def get_zakupy(self):
+        return self.nazwa +"  "+ self.data +"  "+ self.godzina +"  "+ str(self.rabat) + "%"
+
+    def add_product(self,product):
+        self.koszyk.append(product)
+
+    def get_products(self):
+        return self.koszyk
+
+    def zakoncz():
+        pass
+
+
+kauf1 = Zakupy("zakupy świąteczne",16)
+
+kauf1.add_product("jajka")
+kauf1.add_product("mąka")
+kauf1.add_product("mleko")
+
+print(kauf1.get_products())
+
+
