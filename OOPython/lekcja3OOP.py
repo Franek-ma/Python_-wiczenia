e653ed94366ca47a10c7e540c0a7a20caa82e2c5
Franek-ma <malkowskifranek@wp.pl>
2023-04-19 17:30:03 +0200

    zadanie domowe

--- OOPython/lekcja3OOP.py
+++ OOPython/lekcja3OOP.py
@@ -34,29 +34,50 @@ class Zakupy:
         for price in self.koszyk:
             total_price += price.get_price()
         return "wartość koszyka: " + str(total_price)
+    
+    def get_paragon(self):
+        paragon = []
+        total_price = 0
+        total_rabat = 0
+        rabat = total_rabat
+        for rab in self.koszyk:
+            total_rabat += rab.rabat
+        for price in self.koszyk:
+            total_price += price.get_price()
+        names = []
+        for name in self.koszyk:
+            names.append(name.get_name()) 
+        paragon_price = total_price - total_rabat
+        return "produkty: " + str(names) + ", " + "cena produktów: " + str(paragon_price) + "zł" + " data " + self.data + " godzina " + self.godzina + " rabat: " + str(total_rabat) + "zł"
+
+        
+    
 
             
     
 class Product:
 
-    def __init__(self,nazwa,cena):
+    def __init__(self,nazwa,cena,rabat):
 
         self.nazwa = nazwa
         self.cena = cena
+        self.rabat = rabat
 
     def get_price(self):
         return self.cena
     
     def get_name(self):
-        return self.nazwa
+        return self.nazwa   
 
-jajka = Product("jaka wiejkie",12)
+jajka = Product("jaka wiejkie",12,1.70)
 
-chleb = Product("chleb smaczny ", 10)
+chleb = Product("chleb smaczny ", 10,1)
 
-pomidory = Product("malinowe", 20)
+pomidory = Product("malinowe", 20,3)
 
-pomidory2 = Product("jakbłkowe", 45)
+pomidory2 = Product("jakbłkowe", 45,9.80)
+
+global zakupy_dzis
 
 zakupy_dzis = Zakupy("zakupy",20)
 
@@ -70,3 +91,6 @@ print(zakupy_dzis.get_name_products())
 print(zakupy_dzis.get_total_price())
 
 #Stworzyć dodatkową funkcję w klasie Zakupy, która listuje produkty nazwa paragon
+
+print(zakupy_dzis.get_paragon())
+
