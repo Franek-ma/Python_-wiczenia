c3ede3e31bddcbc96a0b54baa17b7859cd66cd74
Franek-ma <malkowskifranek@wp.pl>
2023-04-28 20:14:12 +0200

    dziedziczenie

--- OOPython/dzieczenielekcja1/ddz.py
+++ OOPython/dzieczenielekcja1/ddz.py
@@ -4,5 +4,28 @@ class Samochod:
 
 
 class SamElektryczny(Samochod):
-    def __init__(self,name,wk):
-        self.wk = 
+    def __init__(self,name,wb):
+        self.wb = wb
+        super().__init__(name)
+
+class Spalinowy(Samochod):
+    def __init__(self,name,pojemnosc):
+        self.pojemnosc = pojemnosc
+        super().__init__(name)
+
+
+samochod1 = Spalinowy("samochód",50)
+
+samochod2 = SamElektryczny("samochód_elektryczny",50)
+
+print(samochod1.name)
+
+print(samochod2.name)
+
+#Znaleść cechę wspólną abstrakcyjną dla Broni trzech typów broni
+
+#Klasa nadrzędna dla 3 typów broni muszą dziedzicyć conajmiej 2 cechy po klasie nadrzędnej
+
+#Każda klasa podrzedna muśi mieć ceche swoją
+
+#Każda z tych klas dziedziczy
---
+++ OOPython/dzieczenielekcja1/ddz2.py
@@ -0,0 +1,44 @@
+class Bron:
+    def __init__(self, name, range):
+        super().__init__()
+        self.name = name
+        self.range = range
+
+class Zabawka(Bron):
+    def __init__(self, name, range, bullet):
+        super().__init__(name, range)
+        self.bullet = bullet
+
+class Palna(Bron):
+    def __init__(self, name, range, caliber, recoil):
+        super().__init__(name, range)
+        self.caliber = caliber
+        self.recoil = recoil
+
+class Karabin(Palna):
+    def __init__(self, name, range, caliber, recoil, speed):
+        super().__init__(name, range, caliber, recoil)
+        self.speed = speed
+
+class Shotgun(Palna):
+    def __init__(self, name, range, caliber, recoil, fire_mode):
+        super().__init__(name, range, caliber, recoil)
+        self.fire_mode = fire_mode
+
+
+nerf = Zabawka("Elite", "12m", "foam darts")
+
+ak74 = Palna("ak74","480m","5.70 mm","4500 j")
+
+m4a1 = Karabin("m4a1","520m","5.56 mm","4200 j","on 1 s 5 bullet")
+
+m870 = Shotgun("m870","220 m","8.97 mm","7800 j","single fire")
+
+print(nerf.name) 
+
+print(ak74.caliber)
+
+print(m4a1.recoil) 
+
+print(m870.fire_mode)
+
