04d34003c0e856a8c6a972a74fee3831f4805e16
mathryciuk <mat.hryciuk@gmail.com>
2023-04-28 18:51:38 +0200

    lekcja 3 dziedziczenie

---
+++ OOPython/dzieczenielekcja1/ddz.py
@@ -0,0 +1,8 @@
+class Samochod:
+    def __init__(self,name):
+        self.name = name
+
+
+class SamElektryczny(Samochod):
+    def __init__(self,name,wk):
+        self.wk = 
--- OOPython/dziedzicznie.py
+++ OOPython/dziedzicznie.py
@@ -13,14 +13,32 @@ class Student(Person):
     def get_name(self):
         return "Uczeń : " + self.name
     
+class Student2(Person):
+    def get_name(self):
+        return "Student2" + self.name
+
+
+class Student3(Person):
+    def __init__(self,age,gender,name,python_skills):
+        self.python_skills = python_skills
+        super().__init__(age,gender,name)
+
+    def describe(self):
+        return [self.name,self.age,self.gender,self.python_skills]
 
 
 
 mat = Person(41,"m","Mateusz") 
 fra = Student(13,"m","Franciszek")
+thrd = Student2(13,"m","Dominika")
+
+super1 = Student3(25,"m","Bartek",5)
+
 
 
 print(fra.get_name())
+print(thrd.get_name())
+print(super1.describe())
 
 
 
