cfe14cc9f400f1d55626cf2338d3766291a35956
mathryciuk <mat.hryciuk@gmail.com>
2023-05-13 16:03:41 +0200

    zadanie

---
+++ OOPython/Polimorfizm/task.py
@@ -0,0 +1,7 @@
+# klasa Firma , nazwa , liste urzdzen , nieabstrakcyjna
+# Firma ma urzadzenia - klasa abstrakcyjna - z metoda abstrakcyjan "funkcja_urzadzenia"
+# klasa abstrakcyjna bedzie urzadzenie
+# 3 klasy urzadzen , np , komputer, samochod , traktor ....
+# zaimplementowac funkcje "funkcja_urzadzenia" dla kazdej klasy dziedziczacej
+# stworzyc 3 obiekty klas urzadzen , firma ma funkceje dodaj urzadzenie 
+# firma ma metode , zeby wylistowac jakie mamy urzadzenia i co robia 
