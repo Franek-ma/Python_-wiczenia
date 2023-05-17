8c6a1638ee98801218f7b9ec55d64210375df448
Franek-ma <malkowskifranek@wp.pl>
2023-05-17 18:16:55 +0200

    Zadanie domowe

--- OOPython/Polimorfizm/task.py
+++ OOPython/Polimorfizm/task.py
@@ -5,3 +5,46 @@
 # zaimplementowac funkcje "funkcja_urzadzenia" dla kazdej klasy dziedziczacej
 # stworzyc 3 obiekty klas urzadzen , firma ma funkceje dodaj urzadzenie 
 # firma ma metode , zeby wylistowac jakie mamy urzadzenia i co robia 
+from abc import ABC, abstractmethod
+
+class Firma:
+    def __init__(self, nazwa):
+        self.nazwa = nazwa
+        self.urzadzenia = []
+
+def dodaj_urzadzenie(self, urzadzenie):
+        self.urzadzenia.append(urzadzenie)
+
+def wylistuj_urzadzenia(self):
+        for urzadzenie in self.urzadzenia:
+            print(urzadzenie.opis())
+
+class Urzadzenie(ABC):
+    @abstractmethod
+    def funkcja_urzadzenia(self):
+        pass
+
+class Komputer(Urzadzenie):
+    def funkcja_urzadzenia(self):
+        return "odpalam windows..."
+
+class Samochod(Urzadzenie):
+    def funkcja_urzadzenia(self):
+        return "jeżdze ..."
+    
+class Traktor(Urzadzenie):
+    def funkcja_urzadzenia(self):
+        return "Stoję i się kurzę ..."
+    
+komputer = Komputer()
+samochod = Samochod()
+traktor = Traktor()
+
+moja_firma = Firma("Moja Firma")
+
+moja_firma.dodaj_urzadzenie(komputer)
+moja_firma.dodaj_urzadzenie(samochod)
+moja_firma.dodaj_urzadzenie(traktor)
+
+moja_firma.wylistuj_urzadzenia()
+    
